28742bbe8a2d7797c082b2cdceba2baf3bce577c
Kevin London <kevinlondon@gmail.com>
2013-05-02 20:21:20 -0700

    Modify Fahrenheit application to use heading.

--- K+R/chapters/1/fahrenheit_to_cels.c
+++ K+R/chapters/1/fahrenheit_to_cels.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
-   for fahr = 0, 20, ..., 300 */
+   for fahr = 0, 20, ..., 300; floating-point-version */
 main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;          /* lower limit of temperature scale */
     upper = 300;        /* upper limit */
     step = 20;          /* step size */
 
     fahr = lower;
+
+    printf("Degree Conversion\n");
+    printf("Fahr.\tCelsius\n");
+    printf("---------------\n");
+
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f \t %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
