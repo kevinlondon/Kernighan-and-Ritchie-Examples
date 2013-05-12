88f4b0c96b5908d085bb9256a04099f66d8bc618
Kevin London <kevinlondon@gmail.com>
2013-05-11 19:10:18 -0700

    Transcribed example.

--- K+R/chapters/1/section_190_longest_line.c
+++ K+R/chapters/1/section_190_longest_line.c
@@ -43,3 +43,13 @@ int getline(char s[], int lim)
     return i;
 }
 
+/* copy:   copy 'from' into 'to'; assume 'to' is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
+}
