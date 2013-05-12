4ff773599e7aa63c7d8f1c71ca69f9729b70f7c3
Kevin London <kevinlondon@gmail.com>
2013-05-11 23:20:36 -0700

    Include reverse exercise

---
+++ K+R/chapters/1/1-19/reverse_line.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAXLINE 1000    /* maximum input line length */
+
+/* Ex 1-19: Write a function that reverses the character string s.
+            Use it to write a program that reverse input a line
+            at a time. */
+
+void reverse(char line[], int strlength);
+int getlength(char line[], int limit);
+
+main()
+{
+    int len;
+
+    char line[MAXLINE];
+    char rev_line[MAXLINE];
+
+    while ((len = getlength(line, MAXLINE)) > 0){
+        printf("Original: %s\n", line);
+        reverse(line, len);
+    }
+}
+
+/* reverse: Copy a string in reverse and modify in-place */
+void reverse(char s[], int strlen)
+{
+    int i;
+
+    printf("Reversed: ");
+    for (i=strlen; i >= 0; --i)
+        putchar(s[i]);
+
+    printf("\n");
+}
+
+/* getlength: find length of string, excluding newline at the end. */
+int getlength(char s[], int lim)
+{
+    int c, i;
+
+    for (i=0; i < lim-1 && (c=getchar()) != EOF && c!='\n'; ++i){
+        s[i] = c;
+    }
+    s[i] = '\0';        /* the last character we hit will be a \n. Overwrite. */
+
+    return i;
+}
+
