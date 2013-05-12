65752b6486c42ecca82db7bdd8ba3cb5bfdc73a3
Kevin London <kevinlondon@gmail.com>
2013-05-11 20:54:49 -0700

    Include application to write lines past a minimum length

---
+++ K+R/chapters/1/1-17/print_beyond_80_chars.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define MAXLINE 1000    /* maximum input line length */
+#define MINLINE 81      /* minimum input line length */
+
+int my_getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+main()
+{
+    int len;
+    char line[MAXLINE];
+
+    while ((len = my_getline(line, MAXLINE)) > 0)
+        if (len > MINLINE){
+            printf("Length: %d, %s", len, line);
+        }
+}
+
+/* my_getline:   read a line into s, return length */
+int my_getline(char s[], int lim)
+{
+    int c, i;
+
+    for (i=0; i < lim-1 && (c=getchar()) != EOF && c!='\n'; ++i)
+        s[i] = c;
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
+}
