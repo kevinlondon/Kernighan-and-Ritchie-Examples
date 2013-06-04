a0e5c9e155a680fe4ef3e8dc0dde4452eda5c30a
Kevin London <kevinlondon@gmail.com>
2013-06-03 23:53:44 -0700

    Rough solution to line folding problem, working out details.

--- K+R/chapters/1/1-22/fold_lines.c
+++ K+R/chapters/1/1-22/fold_lines.c
@@ -1,19 +1,17 @@
-/* Exercise 1-22. 
-Write a program to ``fold'' long input lines into two or more shorter 
-lines after the last non-blank character that occurs before the n-th 
-column of input. Make sure your program does something intelligent with 
-very long lines, and if there are no blanks or tabs before the 
+/* Exercise 1-22.
+Write a program to ``fold'' long input lines into two or more shorter
+lines after the last non-blank character that occurs before the n-th
+column of input. Make sure your program does something intelligent with
+very long lines, and if there are no blanks or tabs before the
 specified column. */
 
 #include <stdio.h>
 
 #define MAXLINE     1000        /* maximum input line size */
-#define LINELIM   80          /* Once it reaches this point, wrap it. */
+#define LINELIM   79          /* Once it reaches this point, wrap it. */
 
-int max;                    /* maximum length seen so far */
+int my_getline(char s[]);
 
-int splitline(int space);
-int copy(int split);
 
 /*
 after the last non-blank character
@@ -22,40 +20,53 @@ before nth column of input
 So track an index of the last blank or tab character.
 
 "very long lines" - if buffer fills up, try to split it and process what exists
-"if no blanks or tabs" - try to go up to to 20% more above the length of the line. 
+"if no blanks or tabs" - try to go up to to 20% more above the length of the line.
 if nothing exists, copy as is up to the length of the line. */
 
 main()
 {
     int len;                /* current line length */
-    int c, i;                  
     int space;              /* last seen space index */
     char line[MAXLINE];       /* current input line */
-    char shortline[MAXLINE];  /* split line saved here */
+    int i, count;
 
-    i = 0;
+    space = -1;
 
-    while ((c == getchar()) != EOF) {
-        if (i => LINELIM)
-            i = splitline();
+    while ((len = my_getline(line)) > 0){
+        if (len > LINELIM) {
+            i = 0;
+            count = 0;
+            while (i < len){
+                if (line[i] == ' ' || line[i] == '\t')
+                    space = i;
 
-        if (c == ' ' || c == '\t')
-            space = i;
-        line[i] = c;
+                if (count == LINELIM){
+                    line[space] = '\n';
+                    count = 0;
+                }
+                ++count;
+                ++i;
+            }
+        }
+        printf(line);
+    }
+
     return 0;
 }
 
-/* splitline: split the line at the most convenient place, return new start index */
-int splitline(int space)
+/* my_getline:   read a line into s, return length */
+int my_getline(char s[])
 {
+    int c, i;
 
+    for (i=0; i < MAXLINE-1 && (c=getchar()) != EOF && c!='\n'; ++i)
+        s[i] = c;
 
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
 
+    s[i] = '\0';
+    return i;
 }
-
-/* copy: copy the pieces of the split line */
-int copy(int split)
-{
-    ;
-}
-
